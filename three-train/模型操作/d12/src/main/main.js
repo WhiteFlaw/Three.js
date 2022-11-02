261e6532ce5a7fb828abfe9f7b014e44f967f0e3
WhiteFlaw <71926259+WhiteFlaw@users.noreply.github.com>
2022-11-02 19:28:22 +0800

    Add files via upload

---
+++ three-train/模型操作/d12/src/main/main.js
@@ -0,0 +1,93 @@
+import * as THREE from 'three';
+import { DirectionalLight } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+//创建场景
+const scene = new THREE.Scene();
+
+//创建相机
+const carema = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//设置相机位置
+carema.position.set(0, 0, 10);
+
+//加入相机到场景
+scene.add(carema);
+
+//设置cubetextureloader
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const negx = require("../assets/negx.jpg");
+const negy = require("../assets/negy.jpg");
+const negz = require("../assets/negz.jpg");
+const posx = require("../assets/posx.jpg");  //贴图加不全就不显示贴图, 必需六张
+const posy = require("../assets/posy.jpg");
+const posz = require("../assets/posz.jpg");
+
+const envMapTexture = cubeTextureLoader.load([
+    negx,
+    negy,
+    negz,
+    posx,
+    posy,
+    posz
+])
+
+//创建几何体
+const geometry = new THREE.SphereGeometry(2, 20, 20);  //xyz111的正方体
+
+const material = new THREE.MeshStandardMaterial({
+    metalness: 0.7,
+    roughness: 0.1,
+    envMap: envMapTexture
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//环境光源
+const light = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(light);
+
+//平行光, 可以投射阴影, 但只平行光的话照不到的地方都是黑的
+const directionalLight = new DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(10, 10, 10);//xyz
+scene.add(directionalLight);
+
+//声明构造器(渲染器): WebGLRenderer, 渲染器会渲染出一个画布
+const renderer = new THREE.WebGLRenderer();
+
+//设置渲染尺寸
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+//将渲染完的canvas内容添加到body
+document.body.appendChild(renderer.domElement)
+
+//创建轨道控制器
+const controls = new OrbitControls(carema, renderer.domElement);
+
+//声明坐标系（非必须）
+const axeHelper = new THREE.AxesHelper('20');  //传入坐标轴长度， Str,num均可
+
+//将坐标系添加到场景
+scene.add(axeHelper);
+
+function render() {   //调用render()进行重渲染
+    requestAnimationFrame(render);
+    renderer.render(scene, carema);
+}
+
+render();
+
+window.addEventListener('resize', () => {
+    //更新相机锥体视区长宽比
+    carema.aspect = window.innerWidth / window.innerHeight;
+
+    //手动更新相机的投影矩阵(重新计算投影矩阵), 相机对象的投影矩阵相关属性变化后，再重新计算相机投影矩阵值, 否则会造成资源浪费
+    carema.updateProjectionMatrix();
+
+    //更新渲染器
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    //设置渲染器的像素比
+    renderer.setPixelRatio(window.devicePixelRatio);
+})
